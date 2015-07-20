df0eb61637ea89680ab78541b5cd8976bdebcc01
Karthick Sundararajan <karthicksmail@gmail.com>
2015-07-20 12:41:30 +0800

    Takes command line options. Implemented the option handling without using Getopt

--- CharacterHistogram/perl/character_histogram.pl
+++ CharacterHistogram/perl/character_histogram.pl
@@ -5,6 +5,9 @@ use warnings;
 use Data::Dumper;
 use Getopt::Long;
 
+my ($filename, $shld_cnt_words, $shld_cnt_chars);
+my $USAGE="./character_histogram.pl [-c|-w] <filename>\n";
+
 sub count_words
 {
 	my (%word_count, $word);
@@ -47,28 +50,65 @@ sub print_count_hash
 
 sub main
 {
-	my ($filename, $shld_cnt_words, $shld_cnt_chars) = @_;
-
 	open (FH, $filename) or die "$filename could not be opened";
 
-	if ($shld_cnt_words eq "1")
+	if (defined $shld_cnt_words)
 	{
 		&print_count_hash(&count_words());
 	}
+	seek(FH, 0, 0);
 
-	if ($shld_cnt_chars eq "1")
+	if (defined $shld_cnt_chars)
 	{
 		&print_count_hash(&count_chars());
 	}
 }
 
+sub process_args
+{
+	my (@cmd_line_args) = @_;
 
-#my ($filename, $shld_cnt_words, $shld_cnt_chars);
-my $filename;
+	foreach my $arg (@cmd_line_args)
+	{
+		# if it starts with a -, then it is an option
+		# if it is an option, then it has to have one of the valid characters: c, w
+		# if it is not an option, then it is the name of the file to be processed
+		if ($arg =~ m/^-/)
+		{
+			if ($arg eq "-c")
+			{
+				$shld_cnt_chars = 1;
+				print "Defined Count Chars";
+			}
+			if ($arg eq "-w")
+			{
+				$shld_cnt_words = 1;
+				print "Defined Count Words";
+			}
+			if ($arg eq "-cw" || $arg eq "-wc")
+			{
+				$shld_cnt_words = $shld_cnt_chars = 1;
+				print "Defined Count Chars and Words";
+			}
+		}
+		else
+		{
+			$filename = $arg;
+		}
+	}
 
-GetOptions( "words" => \( my $shld_cnt_words = "0"),
-	    "chars" => \( my $shld_cnt_chars = "0")
-	    );
+	#Check if at least one of the processing options is chosen
+	if (! defined $shld_cnt_words && ! defined $shld_cnt_chars)
+	{
+		print "Define process option\n";
+		print $USAGE;
+	}
+	if (length($filename) == 0)
+	{
+		print $USAGE;
+	}
+}
 
-&main($ARGV[-1], $shld_cnt_words, $shld_cnt_chars);
+&process_args(@ARGV);
+&main;
 
