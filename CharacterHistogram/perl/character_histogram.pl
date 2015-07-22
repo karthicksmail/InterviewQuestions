b2dcf8f99703780441ec03dd5ffadcf916e5cbbf
Karthick Sundararajan <karthicksmail@gmail.com>
2015-07-22 20:14:11 +0800

    Passing the counting process logic as a subroutine reference to generic count_elems method

--- CharacterHistogram/perl/character_histogram.pl
+++ CharacterHistogram/perl/character_histogram.pl
@@ -10,32 +10,35 @@ my $USAGE="./character_histogram.pl [-c|-w] <filename>\n";
 
 sub count_words
 {
-	my (%word_count, $word);
-	while (my $line = <FH>)
+	my ($line) = @_;
+	our (%elem_count, $elem);
+	foreach $elem ($line =~ /\w+/g)
 	{
-		chomp $line;
-		$line = lc($line);
-		foreach $word ($line =~ /\w+/g)
-		{
-			$word_count{$word}++;
-		}
+		$elem_count{$elem}++;
 	}
-	return %word_count;
 }
 
 sub count_chars
 {
-	my (%char_count, $char);
+	my ($line) = @_;
+	our (%elem_count, $elem);
+	foreach $elem (split(//, $line) )
+	{
+		$elem_count{$elem}++ if $elem =~ /[[:alpha:]]/;
+	}
+}
+
+sub count_elems
+{
+	my ($process) = @_;
+	our (%elem_count, $elem);
 	while (my $line = <FH>)
 	{
 		chomp $line;
 		$line = lc($line);
-		foreach $char (split(//, $line) )
-		{
-			$char_count{$char}++ if $char =~ /[[:alpha:]]/;
-		}
+		&$process($line);
 	}
-	return %char_count;
+	return %elem_count;
 }
 
 sub print_count_hash
@@ -54,13 +57,13 @@ sub main
 
 	if (defined $shld_cnt_words)
 	{
-		&print_count_hash(&count_words());
+		&print_count_hash(&count_elems(\&count_words));
 	}
 	seek(FH, 0, 0);
 
 	if (defined $shld_cnt_chars)
 	{
-		&print_count_hash(&count_chars());
+		&print_count_hash(&count_elems(\&count_chars));
 	}
 }
 
@@ -78,17 +81,14 @@ sub process_args
 			if ($arg eq "-c")
 			{
 				$shld_cnt_chars = 1;
-				print "Defined Count Chars";
 			}
 			if ($arg eq "-w")
 			{
 				$shld_cnt_words = 1;
-				print "Defined Count Words";
 			}
 			if ($arg eq "-cw" || $arg eq "-wc")
 			{
 				$shld_cnt_words = $shld_cnt_chars = 1;
-				print "Defined Count Chars and Words";
 			}
 		}
 		else
@@ -100,12 +100,12 @@ sub process_args
 	#Check if at least one of the processing options is chosen
 	if (! defined $shld_cnt_words && ! defined $shld_cnt_chars)
 	{
-		print "Define process option\n";
-		print $USAGE;
+		die $USAGE;
 	}
-	if (length($filename) == 0)
+
+	if (!defined($filename))
 	{
-		print $USAGE;
+		die $USAGE;
 	}
 }
 
